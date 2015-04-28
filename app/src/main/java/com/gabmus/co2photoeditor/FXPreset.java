6cd740629b31cc7af15e895fda4d1f0da672baee
gabmus <emaildigabry@gmail.com>
2015-04-28 12:19:03 +0200

    Initial work on presets Implemented two classes to handle presets

---
+++ app/src/main/java/com/gabmus/co2photoeditor/FXPreset.java
@@ -0,0 +1,30 @@
+package com.gabmus.co2photoeditor;
+
+
+/**
+ * Created by gabmus on 28/04/15.
+ */
+public class FXPreset {
+    private int fxCount;
+    private FXPresetTunings [] presetList;
+    public FXPreset(int fxCount_, int [] fxID_, int [] tuningCount_, int [][] tuningValues_) {
+        fxCount=fxCount_;
+        presetList = new FXPresetTunings[fxCount];
+        for (int i = 0; i < fxCount; i++) {
+            presetList[i] = new FXPresetTunings(fxID_[i], tuningCount_[i], tuningValues_[i]);
+        }
+    }
+
+    public void toggleAllFX(FXHandler mFX, FilterSurfaceView mFsv, boolean active) {
+        mFX.resetAllFX(mFsv);
+        if (active)
+            for (int i = 0; i < fxCount; i++) {
+                mFX.enableFX(presetList[i].fxID, MainActivity.fsv, active);
+                for (int j = 0; j < presetList[i].tuningCount; j++) {
+                    mFX.tuneFX(presetList[i].fxID, j, presetList[i].tuningValues[j], mFsv);
+                }
+            }
+    }
+
+
+}
---
+++ app/src/main/java/com/gabmus/co2photoeditor/FXPresetTunings.java
@@ -0,0 +1,17 @@
+package com.gabmus.co2photoeditor;
+
+/**
+ * Created by gabmus on 28/04/15.
+ */
+public class FXPresetTunings {
+    public int fxID;
+    public int tuningCount;
+    public int [] tuningValues;
+
+    public FXPresetTunings(int fxID_, int tuningCount_, int [] tuningValues_) {
+        fxID=fxID_;
+        tuningCount=tuningCount_;
+        tuningValues=tuningValues_.clone();
+    }
+
+}
