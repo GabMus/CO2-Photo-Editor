862fb138bdf4ac7a016aeca45a586ba72414ad5f
GabMus <emaildigabry@gmail.com>
2015-05-06 19:57:34 +0200

    fixed top back button behaviour on aboutactivity

--- app/src/main/java/com/gabmus/co2photoeditor/AboutActivity.java
+++ app/src/main/java/com/gabmus/co2photoeditor/AboutActivity.java
@@ -63,4 +63,14 @@ public class AboutActivity extends Activity {
         });
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
